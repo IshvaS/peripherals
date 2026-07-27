182171f6fd0ef2873462ea91d8971024d8ac5075
IshvaS <ishva2k6@gmail.com>
2026-07-27 14:14:03 +0530

    Updated peripheral package

--- src/timer/peripherals_pkg.sv
+++ src/timer/peripherals_pkg.sv
@@ -100,3 +100,30 @@ package uart_pkg;
     localparam logic [11:0] REG_SCR = 12'h01C; // BASEREG + 0x1C
 
 endpackage : uart_pkg
+
+// ---------------------------------------------------------
+// Unified Interrupt Controller Register Map
+// ---------------------------------------------------------
+package uic_pkg;
+
+    localparam logic [11:0] REG_IRQ_ENABLE    = 12'h000; // BASEREG + 0x00
+    localparam logic [11:0] REG_IRQ_PENDING   = 12'h004; // BASEREG + 0x04
+    localparam logic [11:0] REG_SET_PENDING   = 12'h008; // BASEREG + 0x08
+    localparam logic [11:0] REG_CLR_PENDING   = 12'h00C; // BASEREG + 0x0C
+
+    localparam int IRQ_COUNT        = 8;
+    
+endpackage : uic_pkg
+
+// ---------------------------------------------------------
+// Interrupt Controller Register Map
+// ---------------------------------------------------------
+package interrupt_pkg;
+
+    localparam logic [11:0] REG_INT_ENABLE        = 12'h000; // BASEREG + 0x00
+    localparam logic [11:0] REG_INT_PENDING       = 12'h004; // BASEREG + 0x04
+    localparam logic [11:0] REG_INT_SET_PENDING   = 12'h008; // BASEREG + 0x08
+    localparam logic [11:0] REG_INT_CLR_PENDING   = 12'h00C; // BASEREG + 0x0C
+    localparam logic [11:0] REG_INT_CLAIM_ID      = 12'h010; // BASEREG + 0x10
+
+endpackage : interrupt_pkg
